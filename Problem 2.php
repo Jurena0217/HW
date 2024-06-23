1ac3b3b3879248cafc6c822ad61f94749541fc35
Jurena0217 <145516924+Jurena0217@users.noreply.github.com>
2024-06-23 10:05:16 -0400

    Add files via upload

---
+++ Problem 2.php
@@ -0,0 +1,55 @@
+<?php
+$a1 = [10.001, 11.591, 0.011, 5.991, 16.121, 0.131, 100.981, 1.001];
+$a2 = [1.99, 1.99, 0.99, 1.99, 0.99, 1.99, 0.99, 0.99];
+$a3 = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
+$a4 = [10.01, -12.22, 0.23, 19.20, -5.13, 3.12];
+function getTotal($arr) {
+    echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
+    $total = 0.00;
+    
+    //TODO do adding here
+    $total = array_sum($arr);
+
+    //TODO do rounding stuff here
+    $total = round($total,2);
+
+
+    echo "The total is $total";
+}
+echo "Problem 2: Adding Floats<br>";
+echo getTotal($a1)
+?>
+<table>
+    <thread>
+        <th>A1</th>
+        <th>A2</th>
+        <th>A3</th>
+        <th>A4</th>
+    </thread>
+    <tbody>
+        <tr>
+            <td>
+                <?php getTotal($a1) ?>
+            </td>
+            <td>
+                <?php getTotal($a2) ?>
+            </td>
+            <td>
+                <?php getTotal($a3) ?>
+            </td>
+            <td>
+                <?php getTotal($a4); ?>
+            </td>
+        </tr>
+</table>
+<style>
+    table {
+        border-spacing: 2em 3em;
+        border-collapse: separate;
+    }
+
+    td {
+        border-right: solid 1px black;
+        border-left: solid 1px black;
+    }
+</style>
---
+++ Problem 3.php
@@ -0,0 +1,59 @@
+<?php
+$a1 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
+$a2 = [-1, 1, -2, 2, 3, -3, -4, 5];
+$a3 = [-0.01, -0.0001, -.15];
+$a4 = ["-1", "2", "-3", "4", "-5", "5", "-6", "6", "-7", "7"];
+
+function bePositive($arr) {
+    echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
+    echo "<br>Positive output:<br>";
+    $output = [];
+    //start edits
+    //note: use the $arr variable, don't directly touch $a1-$a4
+    //TODO Take each value of the $arr, convert it to positive, and set it to the same index in the $output array but with the original data type (i.e., if the source was a string the output slot value should be a string)
+    
+    //end edits
+    
+    //displays the output along with their types
+    $mappedOutput = array_map(function($o) {
+        $type = strtoupper(substr(gettype($o), 0, 1));
+        return "$o ($type)";
+    }, $output);
+    echo implode(', <br>', $mappedOutput);
+}
+echo "Problem 3: Be Positive<br>";
+?>
+<table>
+    <thread>
+        <th>A1</th>
+        <th>A2</th>
+        <th>A3</th>
+        <th>A4</th>
+    </thread>
+    <tbody>
+        <tr>
+            <td>
+                <?php bePositive($a1); ?>
+            </td>
+            <td>
+                <?php bePositive($a2); ?>
+            </td>
+            <td>
+                <?php bePositive($a3); ?>
+            </td>
+            <td>
+                <?php bePositive($a4); ?>
+            </td>
+        </tr>
+</table>
+<style>
+    table {
+        border-spacing: 2em 3em;
+        border-collapse: separate;
+    }
+
+    td {
+        border-right: solid 1px black;
+        border-left: solid 1px black;
+    }
+</style>
